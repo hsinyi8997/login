2f17a2b7926055ef6e8d11ba2b54682818bb0478
hsinyi8997 <hsinyi8997@gmail.com>
2021-11-17 16:45:38 +0800

    feat: add login info varify function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const { engine } = require('express-handlebars')
+const loginList = require('./models/login')
 const app = express()
 const port = 3000
 
@@ -15,12 +16,32 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.use(express.urlencoded({ extended: true }))
 app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body
+  loginList.find()
+    .lean()
+    .then(lists => {
+      const emailInList = lists.find(list => list.email === email)
+      if (!emailInList) {
+        res.render('index', { wrongEmail: email, email, password })
+      } else {
+        if (emailInList.password !== password) {
+          res.render('index', { wrongPassword: password, email: email, password})
+        } else {
+          res.render('success', { name: emailInList.firstName})
+        }
+      }
+    })
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
