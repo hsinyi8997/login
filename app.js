b4118bca7f7866e7f9b3ad503cf7e7224670cce4
hsinyi8997 <hsinyi8997@gmail.com>
2021-11-17 17:03:36 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const { engine } = require('express-handlebars')
 const routes = require('./routes')
-const loginList = require('./models/login')
 const app = express()
 const port = 3000
 
-mongoose.connect('mongodb://localhost/login-list')
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
+require('./config/mongoose')
 app.use(express.urlencoded({ extended: true }))
 app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
--- models/seeds/loginseeder.js
+++ models/seeds/loginseeder.js
@@ -1,12 +1,6 @@
-const mongoose = require('mongoose')
+const db = require('../../config/mongoose')
 const Login = require('../login')
 const loginSample = require('../../loginSample.json')
-mongoose.connect('mongodb://localhost/login-list')
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 
 db.once('open', () => {
   Login.insertMany(loginSample)
